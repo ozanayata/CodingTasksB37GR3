dff2840632f2a443b50d0afdfe13574f88fc5efd
alexandrasorokotyaga <sorokotyagafamily@yahoo.com>
2024-11-14 14:06:49 -0500

    changes1

---
+++ src/AlexandraTaskSolution/OddOrEven.java
@@ -0,0 +1,14 @@
+package AlexandraTaskSolution;
+
+public class OddOrEven {
+
+
+    /*
+    Write  a method which can identifies given number is even or odd
+        EX:
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
+
+     */
+
+}
