733706b130c1dd744b64b241a38fb5be8dfddd89
Ozan Ayata <ozannettim@gmail.com>
2024-11-14 13:17:12 -0500

    task is added

---
+++ src/ozanTaskSolution/OddOrEven.java
@@ -0,0 +1,13 @@
+package ozanTaskSolution;
+
+public class OddOrEven {
+    /*
+    Write  a method which can identifies given number is even or odd
+        EX:
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
+
+     */
+
+
+}
