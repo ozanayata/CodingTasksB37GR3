2cf4f82aa3036d862c822bb86203b97024da1a04
NATALIA MELECA <meleca_natalia@yahoo.com>
2024-11-14 14:00:47 -0500

    created package week01 and added the OddOrEven file

---
+++ src/Natalia_week01/OddOrEven.java
@@ -0,0 +1,11 @@
+package Natalia_week01;
+
+public class OddOrEven {
+    /*
+    Write  a method which can identifies given number is even or odd
+        EX:
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
+
+     */
+}
