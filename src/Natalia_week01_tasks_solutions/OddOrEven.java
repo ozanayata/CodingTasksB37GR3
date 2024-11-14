38c88067b82df1dbbbd9b553d08546599f4e454e
IvannaPashkevych <ivannapashkevych@gmail.com>
2024-11-14 15:03:34 -0500

    created Odd or Even1

--- src/Natalia_week01_tasks_solutions/OddOrEven.java
+++ src/Natalia_week01_tasks_solutions/OddOrEven.java
@@ -1,13 +1,11 @@
-package ivannaTaskSolution;
+package Natalia_week01_tasks_solutions;
 
 public class OddOrEven {
-    /*) ->  "Odd"
-        identify(6)
+    /*
     Write  a method which can identifies given number is even or odd
         EX:
-        identify(5->  "Even"
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
 
      */
-
-
 }
