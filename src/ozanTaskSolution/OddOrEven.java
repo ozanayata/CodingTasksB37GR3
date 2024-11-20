4e27f1f69e893a00671a06a73e878f0492ac9644
Ozan Ayata <ozannettim@gmail.com>
2024-11-19 21:48:41 -0500

    task is solution is added

--- src/ozanTaskSolution/OddOrEven.java
+++ src/ozanTaskSolution/OddOrEven.java
@@ -1,6 +1,22 @@
 package ozanTaskSolution;
 
 public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        System.out.println("5 is an " + oddOrEven(5) + " number");
+        System.out.println("6 is an " + oddOrEven(6) + " number");
+
+
+    }
+    public static String oddOrEven(int num) {
+        if(num%2==0){
+            return "Even";
+        }else{
+            return "Odd";
+        }
+    }
+}
     /*
     Write  a method which can identifies given number is even or odd
         EX:
@@ -10,4 +26,4 @@ public class OddOrEven {
      */
 
 
-}
+
