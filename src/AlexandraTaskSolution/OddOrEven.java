ef29f09cef98229e953447ccccf77e8e53b24b31
alexandrasorokotyaga <sorokotyagafamily@yahoo.com>
2024-11-20 14:40:15 -0500

    OddOrEven method#2

--- src/AlexandraTaskSolution/OddOrEven.java
+++ src/AlexandraTaskSolution/OddOrEven.java
@@ -2,16 +2,30 @@ package AlexandraTaskSolution;
 
 public class OddOrEven {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 6;
 
-        if (num1 % 2 == 1) {
-            System.out.println(num1 + " is odd");
+
+        int num = 5;
+        int num1 = 6;
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
         } else {
-            System.out.println(num1 + " is even");
+            System.out.println(num + " is odd");;
         }
+        if (num1 % 2 == 0) {
+    System.out.println(num1 + " is even");
+       } else {
+    System.out.println(num1 + " is odd");;
+       }
+    }
+}
 
-            if (num2 % 2 == 2) {
+
+//} else {
+//System.out.println(num2 + " is even");
+//System.out.println(num2 + " is odd");
+//} else if (num2 % 2 == 1) {
+           /* if (num2 % 2 == 1) {
                 System.out.println(num2 + " is odd");
             } else {
                 System.out.println(num2 + " is even");
