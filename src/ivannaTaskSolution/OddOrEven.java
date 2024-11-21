56d18589e76bd6c9f096b9f7263ecb2e9f404a0d
IvannaPashkevych <ivannapashkevych@gmail.com>
2024-11-21 14:06:36 -0500

    created task solution odd or even

--- src/ivannaTaskSolution/OddOrEven.java
+++ src/ivannaTaskSolution/OddOrEven.java
@@ -2,7 +2,6 @@ package ivannaTaskSolution;
 
 public class OddOrEven {
 
-
     /*
     Write  a method which can identifies given number is even or odd
         EX:
@@ -10,5 +9,18 @@ public class OddOrEven {
         identify(6) ->  "Even"
 
      */
+    public static void main(String[] args) {
+        System.out.println("15 is an " + oddOrEven(15));
+        System.out.println("1987 is an " + oddOrEven(1987));
+        System.out.println("8 is an " + oddOrEven(8));
 
+    }
+
+    public static String oddOrEven(int num){
+        if (num % 2 == 0){
+            return "even";
+        }else {
+            return "odd";
+        }
+    }
 }
