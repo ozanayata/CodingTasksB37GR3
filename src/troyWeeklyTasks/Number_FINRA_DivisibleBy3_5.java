f5832f3d64d8c25b6f432f63c22fd16633816a28
TroyYC <troy@cydeo.com>
2024-11-21 14:30:23 -0500

    week2 task shared

---
+++ src/troyWeeklyTasks/Number_FINRA_DivisibleBy3_5.java
@@ -0,0 +1,24 @@
+package troyWeeklyTasks;
+
+public class Number_FINRA_DivisibleBy3_5 {
+
+    /*
+    Write a function which prints out the numbers from 1 to 30 but
+    for numbers which are a multiple of 3, print "FIN" instead of the number
+    and for numbers which are a multiple of 5, print "RA" instead of the number.
+    for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the number.
+
+    EX:
+    1 2 FIN 4 RA FIN 7 8 FIN RA 11 FIN 13 14 FINRA 16 17 FIN 19 RA FIN 22 23 FIN RA 26 FIN 28 29 FINRA
+     */
+
+    //  Tara will share the solution
+    // next week we will meet on Wednesday at 3:30 pm EST
+
+
+
+
+
+
+
+}
