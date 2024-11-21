4f577d961246a89c36ebba1649232d002802fb3e
tlbailey82 <tlbailey82@gmail.com>
2024-11-21 16:08:24 -0500

    Finished the FINRA task

--- src/Tara_tasks/Number_FINRA_DivisibleBy3_5.java
+++ src/Tara_tasks/Number_FINRA_DivisibleBy3_5.java
@@ -1,7 +1,27 @@
 package Tara_tasks;
 
+
+
 public class Number_FINRA_DivisibleBy3_5 {
     public static void main(String[] args) {
+
+        for (int i = 1; i <=30; i++){//the for loop will iterate through nums 1-30
+            //the i represents the even numbers in the sequence
+
+            boolean Div3 = i % 3 ==0;//num div by 3 will return FIN
+            boolean Div5 = i % 5 ==0;//num div by 5 will return RA
+
+            if (Div3 && Div5){//If nums are div by 3 & 5 print FINRA
+                System.out.println("FINRA");
+            } else if (Div3) {//use else if when you want to check multiple conditions
+                System.out.println("FIN");
+            } else if (Div5) {
+                System.out.println("RA");
+            }else {
+                System.out.println(i);//otherwise print out the even numbers
+            }
+        }
+
 
     }
 }
