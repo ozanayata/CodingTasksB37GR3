d0d882bdc9a48a92ed8f4da5d5eb4a1ee4c3d99e
Cihat <cihatusa@gmail.com>
2024-11-21 12:30:30 -0500

    cihat's tasks1

--- src/Cihat_tasks/OddOrEven.java
+++ src/Cihat_tasks/OddOrEven.java
@@ -1,11 +1,25 @@
 package Cihat_tasks;
 
 public class OddOrEven {
-     /*
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        if (num % 2 == 0) {
+                System.out.println("Even");
+            } else {
+                System.out.println("Odd");
+            }
+        }
+
+    }
+
+
+    /*
     Write  a method which can identifies given number is even or odd
         EX:
         identify(5) ->  "Odd"
         identify(6) ->  "Even"
 
      */
-}
+
