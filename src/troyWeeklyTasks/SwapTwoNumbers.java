1e440fc26d2e7ec5ec2f7e37d8e254dc017ba249
IvannaPashkevych <ivannapashkevych@gmail.com>
2024-12-05 13:03:58 -0500

    swap two numbers task solution is sheared

--- src/troyWeeklyTasks/SwapTwoNumbers.java
+++ src/troyWeeklyTasks/SwapTwoNumbers.java
@@ -11,12 +11,22 @@ public class SwapTwoNumbers {
 
    System.out.println("a = " + a); //=> a = 7
    System.out.println("b = " + b); //=> b = 5
-
-
-
-
-
       Alvaro is volunteer for this
 
      */
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 7;
+
+        // Swap without using a third variable
+        a = a + b; // a becomes 12 (5 + 7)
+        b = a - b; // b becomes 5 (12 - 7)
+        a = a - b; // a becomes 7 (12 - 5)
+
+        System.out.println("a = " + a); // a = 7
+        System.out.println("b = " + b); // b = 5
+
+    }
 }
