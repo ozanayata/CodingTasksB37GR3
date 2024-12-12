055971ddaea60d9e9316092eb064a90a6a878406
TroyYC <troy@cydeo.com>
2024-12-12 13:28:12 -0500

    week4 solution and week5 task added

---
+++ src/troyWeeklyTasks/FrequencyOfChars_W5.java
@@ -0,0 +1,16 @@
+package troyWeeklyTasks;
+
+public class FrequencyOfChars_W5 {
+
+/*
+    Write function that can find the frequency of characters
+
+    Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+
+
+    Alexandra is volunteer
+*/
+
+
+
+}
