7801c2ff645fbfaa777a900df2fbb7669b7da595
TroyYC <troy@cydeo.com>
2025-01-03 10:23:09 -0500

    week5 solution and week6 task added

--- src/troyWeeklyTasks/FrequencyOfChars_W5.java
+++ src/troyWeeklyTasks/FrequencyOfChars_W5.java
@@ -11,6 +11,30 @@ public class FrequencyOfChars_W5 {
     Alexandra is volunteer
 */
 
+    public static String frequencyOfChars1(String str) {
+        String expectedResult = "";
+        int j = 0;
+        while (j < str.length()) {//bbbcccdddEEEFFF
+            int count = 0;
+
+            for (int i = 0; i < str.length(); i++) {//bbbcccdddEEEFFF
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;//3
+                }
+            }
+            expectedResult += str.charAt(j) + "" + count;//a3
+            str = str.replace("" + str.charAt(j), "");
+            System.out.println(str);
+
+        }
+        return expectedResult;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(frequencyOfChars1("bbbcccdddEEEFFF"));
+    }
+
 
 
 }
---
+++ src/troyWeeklyTasks/RemoveDuplicates_W6.java
@@ -0,0 +1,12 @@
+package troyWeeklyTasks;
+
+public class RemoveDuplicates_W6 {
+
+     /*
+    Write function that can remove the duplicated values from String
+            Ex: removeDup("AAABBBCCC") ==> ABC
+     */
+     //Natalia is volunteer
+
+
+}
