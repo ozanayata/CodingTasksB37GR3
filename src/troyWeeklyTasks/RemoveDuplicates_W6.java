17a2e3ddfef6ff6644f1967ebfb214469c01ced2
TroyYC <troy@cydeo.com>
2025-01-09 13:11:48 -0500

    week6 solution and week7 task added

--- src/troyWeeklyTasks/RemoveDuplicates_W6.java
+++ src/troyWeeklyTasks/RemoveDuplicates_W6.java
@@ -9,4 +9,18 @@ public class RemoveDuplicates_W6 {
      //Natalia is volunteer
 
 
+    public static String removeDup(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!result.contains("" + str.charAt(i))) {
+                result += "" + str.charAt(i);
+            }
+        }
+
+        return result;
+    }
+
+
+
 }
