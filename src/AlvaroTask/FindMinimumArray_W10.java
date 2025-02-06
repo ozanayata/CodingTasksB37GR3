7a0392dfcf4e11d03e7d1ca88fadc47103019124
Alvaro Lazo <alamon30@outlook.com>
2025-02-06 12:54:09 -0500

    Task 10 review

---
+++ src/AlvaroTask/FindMinimumArray_W10.java
@@ -0,0 +1,30 @@
+package AlvaroTask;
+
+public class FindMinimumArray_W10 {
+
+     /*
+    Write a function that can find the minimum number from an int Array
+     */
+
+    public static void main(String[] args) {
+
+        int [] numbers = {10,20,30,40,50,60,70,80,90};
+
+        System.out.println("findMinimum(numbers) = " + findMinimum(numbers));
+
+    }
+
+    public static int findMinimum(int[] array) {
+        int min = array[0];
+
+        for (int num : array) {
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        return min;
+    }
+
+
+}
