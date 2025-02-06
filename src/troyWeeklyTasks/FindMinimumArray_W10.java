5a3d7f3e23c31fa4cef736cd28c5e5caf53c4297
TroyYC <troy@cydeo.com>
2025-02-06 13:12:08 -0500

    week10 solution and week11 task added

--- src/troyWeeklyTasks/FindMinimumArray_W10.java
+++ src/troyWeeklyTasks/FindMinimumArray_W10.java
@@ -8,4 +8,26 @@ public class FindMinimumArray_W10 {
 
 
     // Ihor is volunteer
+
+    public static int minValue(int[] n) {
+
+        int min = Integer.MAX_VALUE;
+        // OR
+        //int min = n[0];
+
+        for (int each : n) {
+            if (each < min) {
+                min = each;
+            }
+        }
+
+        return min;
+
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1, 5, 8, 3, 2, 11, -1};
+
+        System.out.println(minValue(arr));
+    }
 }
